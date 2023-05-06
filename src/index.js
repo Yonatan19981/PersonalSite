441dd74acfae6387a1dc18f7aa28b4c207bd916e
Yonatan19981 <60484385+Yonatan19981@users.noreply.github.com>
2023-05-06 13:56:10 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,15 +7,6 @@ import ReactGA from 'react-ga';
 ReactGA.initialize('G-LCYDLBGL8E');
 ReactDOM.render(
 	<React.StrictMode>
-	<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-LCYDLBGL8E"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-LCYDLBGL8E');
-</script>
 		<App />
 	</React.StrictMode>,
 	document.getElementById("root"),
